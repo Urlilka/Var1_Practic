21b740c23a2992526fd579ad8b00a0af6d6f034a
Anton Giliov <Urlilka@yandex.ru>
2025-11-06 09:23:03 +0500

    Изменение Views/LoginView.py. Добавлена кнопка, Установлен чёткий размер

--- Views/LoginView.py
+++ Views/LoginView.py
@@ -10,27 +10,31 @@ class LoginView(Tk):
 
         # основное окно, его размеры и тема
         self.title("Информационная система")
-        self.geometry("600x400")
-        self.minsize(400,200)
-        self.maxsize(800,600)
+        self.geometry("400x300")
+        self.resizable(False,False)
         ttk.Style().theme_use("clam")
 
         # Поле ввода логина и подпись
         self.login_Lable = ttk.Label(self, text="Ввод Логин", foreground="#ff3366")
-        self.login_Lable.pack(anchor="center",pady=5, padx=15)
+        self.login_Lable.pack(anchor="center", pady=10)
 
         self.login_Entry = ttk.Entry(self, foreground="#ff0099")
-        self.login_Entry.pack(anchor="center",pady=2, padx=15)
+        self.login_Entry.pack(anchor="center")
         # Поле ввода пароля и подпись
         self.password_Lable = ttk.Label(self, text="Ввод Пароль", foreground="#ff3366")
-        self.password_Lable.pack(anchor="center",pady=5, padx=15)
+        self.password_Lable.pack(anchor="center", pady=10)
 
         self.password_Entry = ttk.Entry(self, foreground="#ff0099")
-        self.password_Entry.pack(anchor="center", pady=2, padx=15)
+        self.password_Entry.pack(anchor="center")
         # Текст (всплывающий)
 
         # Кнопка входа
+        self.enter_Button = ttk.Button(self, text="Вход", command = self.LoginFunc)
+        self.bind("<Key-Return>", lambda e: self.LoginFunc())
+        self.enter_Button.pack(anchor="center",expand=1)
 
+    def LoginFunc(self):
+        print("nya?")
 
 
 if __name__ == "__main__":
