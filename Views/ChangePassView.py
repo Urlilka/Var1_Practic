93bef5dc96bd30f6313263ab9bfa35ed72900d28
Anton Giliov <Urlilka@yandex.ru>
2025-11-07 09:51:14 +0500

    Создание окна первоночального изменения пароля - Views/ChangePassView.py

---
+++ Views/ChangePassView.py
@@ -0,0 +1,9 @@
+from tkinter import ttk, Tk
+from tkinter import *
+
+from Controllers.UserControllers import UserController
+
+
+class ChangePassView(TK):
+    def __init__(self):
+        super().__init__()
