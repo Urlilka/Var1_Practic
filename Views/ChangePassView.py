055c09416a3b7b093daaea308d36c7bdc60e4b25
Anton Giliov <Urlilka@yandex.ru>
2025-11-08 09:54:35 +0500

    Переход в Views/ChangePassView.py и его создание. Работа над функцией первоночальной смены пароля

--- Views/ChangePassView.py
+++ Views/ChangePassView.py
@@ -4,6 +4,75 @@ from tkinter import *
 from Controllers.UserControllers import UserController
 
 
-class ChangePassView(TK):
-    def __init__(self):
+class ChangePassView(Tk):
+    """Окно Изменения Пароля и его функции
+    """
+    def __init__(self, login):
         super().__init__()
+
+        self.user = UserController.show(login)
+
+        self.title(f"Изменение пароля: {self.user.login}")
+        self.geometry("350x350")
+        self.resizable(False,False)
+        ttk.Style().theme_use("clam")
+
+
+        # Ввод старого пароля
+        self.Old_Password_Lable = ttk.Label(self, text="Ввод Старый Пароль", foreground="#ff3366")
+        self.Old_Password_Lable.pack(anchor="center", pady=10)
+
+        self.Old_Password_Entry = ttk.Entry(self, foreground="#ff0099")
+        self.Old_Password_Entry.pack(anchor="center")
+
+
+
+        # Ввод нового пароля
+        self.New_Password_Lable = ttk.Label(self, text="Ввод Старый Пароль", foreground="#ff3366")
+        self.New_Password_Lable.pack(anchor="center", pady=10)
+
+        self.New_Password_Entry = ttk.Entry(self, foreground="#ff0099")
+        self.New_Password_Entry.pack(anchor="center")
+
+
+        # Ввод подтверждения нового пароля
+        self.Conf_New_Password_Lable = ttk.Label(self, text="Ввод Старый Пароль", foreground="#ff3366")
+        self.Conf_New_Password_Lable.pack(anchor="center", pady=10)
+
+        self.Conf_New_Password_Entry = ttk.Entry(self, foreground="#ff0099")
+        self.Conf_New_Password_Entry.pack(anchor="center")
+
+
+
+        # Текст (всплывающий)
+        self.message_Lable = ttk.Label(self, foreground="#ff0099")
+        self.message_Lable.pack(anchor="center", expand=1)
+
+
+        # Кнопка Изменения Пароля
+        self.enter_Button = ttk.Button(self, text="Изменить Пароль", command = self.Change_Pass)
+        self.bind("<Return>", lambda e: self.Change_Pass())
+        self.enter_Button.pack(anchor="center",expand=1)
+
+
+        # Кнопка выхода
+        self.escape_Button = ttk.Button(self, text="Выход", command = self.Escape)
+        self.bind("<Escape>", lambda e: self.Escape())
+        self.enter_Button.pack(anchor="center",expand=1)
+    
+
+    def Change_Pass(self):
+        old_pass = self.Old_Password_Entry.get()
+        new_pass = self.New_Password_Entry.get()
+        conf_new_pass = self.Conf_New_Password_Entry.get()
+
+
+        if old_pass == self.user.password:
+            print("Есть") 
+
+    def Escape(self):
+        self.destroy()
+
+if __name__ == "__main__":
+    window = ChangePassView("test")
+    window.mainloop()
--- Views/LoginView.py
+++ Views/LoginView.py
@@ -3,40 +3,53 @@ from tkinter import *
 
 from Controllers.UserControllers import  *
 
+from Views.ChangePassView import ChangePassView
+
 
 class LoginView(Tk):
+    """Окно Авторизации и его функции
+    """
     def __init__(self):
         super().__init__()
 
+        
         # основное окно, его размеры и тема
         self.title("Информационная система")
         self.geometry("400x300")
         self.resizable(False,False)
         ttk.Style().theme_use("clam")
 
+        
         # Поле ввода логина и подпись
         self.login_Lable = ttk.Label(self, text="Ввод Логин", foreground="#ff3366")
         self.login_Lable.pack(anchor="center", pady=10)
 
         self.login_Entry = ttk.Entry(self, foreground="#ff0099")
         self.login_Entry.pack(anchor="center")
+        
+        
         # Поле ввода пароля и подпись
         self.password_Lable = ttk.Label(self, text="Ввод Пароль", foreground="#ff3366")
         self.password_Lable.pack(anchor="center", pady=10)
 
-        self.password_Entry = ttk.Entry(self, foreground="#ff0099")
+        self.password_Entry = ttk.Entry(self, show="*", foreground="#ff0099")
         self.password_Entry.pack(anchor="center")
+        
+        
         # Текст (всплывающий)
-
         self.message_Lable = ttk.Label(self, foreground="#ff0099")
         self.message_Lable.pack(anchor="center", expand=1)
 
+
         # Кнопка входа
         self.enter_Button = ttk.Button(self, text="Вход", command = self.LoginFunc)
-        self.bind("<Key-Return>", lambda e: self.LoginFunc())
+        self.bind("<Return>", lambda e: self.LoginFunc())
         self.enter_Button.pack(anchor="center",expand=1)
 
+
         self.counter_for_ban = {}
+
+
 
     def LoginFunc(self):
         """Функция Входа с проверкой на разные случаи и функцией бана пользователя.
@@ -68,7 +81,7 @@ class LoginView(Tk):
             elif user.first_auth == 1:
                 self.counter_for_ban[login] = 0
                 self.message_Lable["text"] = f"Вход в окно Изменения Пароля"
-                print("переход в изменение пароля")
+                ChangePassWindow = ChangePassView(user.login)
 
             elif user.date_auth is not None and (datetime.now().date() - user.date_auth).days >= 31:
                 self.counter_for_ban[login] = 0
