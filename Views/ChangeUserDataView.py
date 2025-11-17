59acbb7b9164439c26f3560defbcf45947505998
Anton Giliov <Urlilka@yandex.ru>
2025-11-17 12:58:26 +0500

    Работа над Views/ChangeUserDataView.py -- создание визуала и заготовка под функции

--- Views/ChangeUserDataView.py
+++ Views/ChangeUserDataView.py
@@ -10,9 +10,38 @@ class ChangeUserDataView(Tk):
         self.user = UserController.show(login)
 
         self.title(f"Изменение пользователя: {self.user.login}")
-        self.geometry("450x350")
+        self.geometry("450x400")
         self.resizable(False,False)
         ttk.Style().theme_use("clam")
+
+        # Меняем имя
+        self.Change_name_Label = ttk.Label(self,text="Логин", foreground="#ff3366").pack(anchor="center",pady=[10,2])
+        self.Change_name_Entry = ttk.Entry(self, foreground="#ff0099").pack(anchor="center",pady=[0,15])
+
+        # Меняем пароль
+        self.Change_password_Label = ttk.Label(self,text="Пароль", foreground="#ff3366").pack(anchor="center",pady=[0,2])
+        self.Change_password_Entry = ttk.Entry(self, foreground="#ff0099").pack(anchor="center",pady=[0,30])
+
+        # Применить изменения
+        self.Apply_changes_Button = ttk.Button(self, text="Применить изменения").pack(anchor="center",pady=[0,30])
+
+        # Разбан
+        self.Change_ban_Button = ttk.Button(self, text="Изменение блокировки").pack(anchor="center",pady=[0,30])
+
+        # Сброс даты
+        self.Change_data_Button = ttk.Button(self, text="Сброс Даты").pack(anchor="center", pady=[0,30])
+
+        # Сообщения от системы пользователю
+        self.message_Label = ttk.Label(self,text="_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_", foreground="#ff3366").pack(anchor="center")
+
+        def Change_user(self):
+            pass
+
+        def Change_ban(self):
+            pass
+
+        def Change_data(self):
+            pass
 
 
 if __name__ == "__main__":
